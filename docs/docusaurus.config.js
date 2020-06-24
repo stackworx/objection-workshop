4257cac9b1f071ac2cfcc86160465b7c6cf337f1
cliedeman <ciaran@stackworx.io>
2020-06-24 12:01:25 +0200

    fixed url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,61 +1,61 @@
 module.exports = {
-  title: "Objection Workshop",
-  tagline: "Introduction course into Objection.js",
-  url: "https://stackworx.github.io/",
-  baseUrl: "/objection-workshop/",
-  favicon: "img/favicon.ico",
-  organizationName: "stackworx",
-  projectName: "objection-workshop",
+  title: 'Objection Workshop',
+  tagline: 'Introduction course into Objection.js',
+  url: 'https://stackworx.github.io',
+  baseUrl: '/objection-workshop/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'stackworx',
+  projectName: 'objection-workshop',
   scripts: [
     {
-      src: "/objection-workshop/js/fix-location.js",
+      src: '/objection-workshop/js/fix-location.js',
       async: false,
       defer: false,
     },
   ],
   themeConfig: {
     navbar: {
-      title: "Stackworx Objection Workshop",
+      title: 'Stackworx Objection Workshop',
       logo: {
-        alt: "Logo",
-        src: "img/knexjs.png",
+        alt: 'Logo',
+        src: 'img/knexjs.png',
       },
       links: [
         {
-          to: "docs/getting-started",
-          activeBasePath: "docs",
-          label: "Getting Started",
-          position: "left",
+          to: 'docs/getting-started',
+          activeBasePath: 'docs',
+          label: 'Getting Started',
+          position: 'left',
         },
         {
-          href: "https://github.com/stackworx/objection-workshop",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/stackworx/objection-workshop',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Assignments",
+          title: 'Assignments',
           items: [
             {
-              label: "Kubernetes Tutorials",
-              to: "https://kubernetes.io/docs/tutorials/",
+              label: 'Kubernetes Tutorials',
+              to: 'https://kubernetes.io/docs/tutorials/',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
             {
-              label: "Website",
-              to: "https://stackworx.io/",
+              label: 'Website',
+              to: 'https://stackworx.io/',
             },
             {
-              label: "GitHub",
-              href: "https://github.com/stackworx",
+              label: 'GitHub',
+              href: 'https://github.com/stackworx',
             },
           ],
         },
@@ -65,22 +65,16 @@ module.exports = {
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/stackworx/objection-workshop/edit/master/docs/",
+            'https://github.com/stackworx/objection-workshop/edit/master/docs/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   editUrl:
-        //     "https://github.com/stackworx/objection-workshop/edit/master/website/blog/",
-        // },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
